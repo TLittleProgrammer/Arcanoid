95dc04d0b79fd23d5651aa70504c31a8dae6b084
Danila Bladyko <danila.bladyko@stark.games>
2024-05-08 00:49:29 +0300

    scene-managment Добавил загрузчик сцен

---
+++ Assets/Scripts/External/SceneManagment/ISceneManagementService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+
+namespace External.SceneManagment
+{
+    public interface ISceneManagementService
+    {
+        float SceneLoadingProgress { get; }
+        UniTask LoadSceneAsync(string sceneName, Action sceneLoaded);
+    }
+}
---
+++ Assets/Scripts/External/SceneManagment/SceneManagmentService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace External.SceneManagment
+{
+    public class SceneManagementService : ISceneManagementService
+    {
+        private float _sceneLoadingProgress;
+        
+        public async UniTask LoadSceneAsync(string sceneName, Action sceneLoaded)
+        {
+            _sceneLoadingProgress = 0f;
+
+            AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneName);
+
+            while (!asyncOperation.isDone)
+            {
+                _sceneLoadingProgress = asyncOperation.progress;
+            }
+
+            sceneLoaded?.Invoke();
+
+            await UniTask.CompletedTask;
+        }
+
+        public float SceneLoadingProgress => _sceneLoadingProgress;
+    }
+}
