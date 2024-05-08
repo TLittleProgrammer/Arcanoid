6e7d83d3ec620b90d0bd8e30aebfe9d3e6cac5f6
Danila Bladyko <danila.bladyko@stark.games>
2024-05-08 13:34:33 +0300

    Добавил "кисточку" в редактор

--- Assets/Scripts/Editor/LevelEditor/LevelGeometry.cs
+++ Assets/Scripts/Editor/LevelEditor/LevelGeometry.cs
@@ -7,15 +7,29 @@ using Sirenix.Utilities.Editor;
 using Unity.Mathematics;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Editor.LevelEditor
 {
     public class LevelGeometry : OdinEditorWindow
     {
+        //Geometry
+        [TabGroup("Editor", "Geometry", SdfIconType.Map, TextColor = "green")]
         [TableMatrix(HorizontalTitle = "Level Grid", DrawElementMethod = "DrawElement", ResizableColumns = false, RowHeight = 32)]
         public EntityProvider[,] Grid;
+        
+        
+        //Parameters
+        [TabGroup("Editor", "Level Parameters", SdfIconType.Magic, TextColor = "orange")]
         [OnValueChanged("CreateGrid")]
         public int2 GridSize;
+        
+        
+        [TabGroup("Editor", "Level Parameters", SdfIconType.Magic, TextColor = "orange")]
+        [PreviewField(75)]
+        public EntityProvider EntityProvider;
+
+        private bool _brushIsEnabled = false;
 
         [MenuItem("Tools/Level Editor")]
         private static void OpenWindow()
@@ -28,25 +42,50 @@ namespace Editor.LevelEditor
         {
             Grid = new EntityProvider[GridSize.x, GridSize.y];
         }
-        
+
         private EntityProvider DrawElement(Rect rect, EntityProvider value)
         {
-            if (value is not null)
+            if (Event.current.type == EventType.MouseDown && rect.Contains(Event.current.mousePosition))
             {
-                return (EntityProvider)SirenixEditorFields.UnityPreviewObjectField(
-                    rect: rect,
-                    value: value,
-                    texture: value.EntityStages[0].Sprite.texture,
-                    type: typeof(EntityProvider)
-                );
+                GUI.changed = true;
+                Event.current.Use();
+                
+                if (_brushIsEnabled)
+                {
+                    value = EntityProvider;
+                    return (EntityProvider)SirenixEditorFields.UnityPreviewObjectField(
+                        rect: rect,
+                        value: value,
+                        texture: value.EntityStages[0].Sprite.texture,
+                        type: typeof(EntityProvider)
+                    );
+                }
             }
 
             return (EntityProvider)SirenixEditorFields.UnityPreviewObjectField(
-                    rect: rect,
-                    value: default,
-                    texture: default,
-                    type: typeof(EntityProvider)
-                );
-        } 
+                rect: rect,
+                value: value,
+                texture: (value is null ? default : value.EntityStages[0].Sprite.texture),
+                type: typeof(EntityProvider)
+            );
+        }
+
+        [TabGroup("Editor", "Level Parameters", SdfIconType.Magic, TextColor = "orange")]
+        [ShowIf("_brushIsEnabled")]
+        [HorizontalGroup("Editor/Split", 0.5f)]
+        [Button(SdfIconType.BrushFill, IconAlignment = IconAlignment.LeftOfText, Name = "Brush"), GUIColor(0.4f, 0.8f, 1)]
+        private void OnBrushButtonClickedEnabled()
+        {
+            _brushIsEnabled = false;
+        }
+        
+        [TabGroup("Editor", "Level Parameters", SdfIconType.Magic, TextColor = "orange")]
+        [HideIf("_brushIsEnabled")]
+        [HorizontalGroup("Editor/Split", 0.5f)]
+        [Button(SdfIconType.BrushFill, IconAlignment = IconAlignment.LeftOfText, Name = "Brush")]
+        private void OnBrushButtonClickedDisabled()
+        {
+            _brushIsEnabled = true;
+        }
     }
 }
