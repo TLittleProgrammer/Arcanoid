be903b81e40c7f6be79766902207090d4d23db45
Danila Bladyko <danila.bladyko@stark.games>
2024-05-22 12:20:43 +0300

    Отрефакторил фабрику для LevelItem'ов

--- Assets/App/Scripts/Scenes/MainMenuScene/Factories/Levels/LevelItemFactory.cs
+++ Assets/App/Scripts/Scenes/MainMenuScene/Factories/Levels/LevelItemFactory.cs
@@ -19,125 +19,23 @@ namespace App.Scripts.Scenes.MainMenuScene.Factories.Levels
     {
         private readonly ILevelItemView _prefab;
         private readonly ITransformable _prefabParent;
-        private readonly IUserDataContainer _userDataContainer;
-        private readonly LevelItemViewByTypeProvider _levelItemViewByTypeProvider;
-        private readonly IStateMachine _stateMachine;
-        private readonly LevelPackProvider _levelPackProvider;
-        private readonly ILevelPackInfoService _levelPackInfoService;
         private readonly DiContainer _diContainer;
-        private readonly IEnergyService _energyService;
 
         public LevelItemFactory(
             ILevelItemView prefab,
             ITransformable prefabParent,
-            IUserDataContainer userDataContainer,
-            LevelItemViewByTypeProvider levelItemViewByTypeProvider,
-            IStateMachine stateMachine,
-            LevelPackProvider levelPackProvider,
-            ILevelPackInfoService levelPackInfoService,
-            DiContainer diContainer,
-            IEnergyService energyService)
+            DiContainer diContainer)
         {
             _prefab = prefab;
             _prefabParent = prefabParent;
-            _userDataContainer = userDataContainer;
-            _levelItemViewByTypeProvider = levelItemViewByTypeProvider;
-            _stateMachine = stateMachine;
-            _levelPackProvider = levelPackProvider;
-            _levelPackInfoService = levelPackInfoService;
             _diContainer = diContainer;
-            _energyService = energyService;
         }
         
         public ILevelItemView Create(int packIndex, LevelPack levelPack)
         {
             ILevelItemView levelItemView = _diContainer.InstantiatePrefab(_prefab.GameObject, _prefabParent.Transform).GetComponent<ILevelItemView>();
 
-            LevelPackProgressDictionary packProgress = (LevelPackProgressDictionary)_userDataContainer.GetData<LevelPackProgressDictionary>();
-            
-            VisualTypeId visualType = GetVisualType(packIndex, levelPack, packProgress);
-            
-            ChangeVisual(packIndex, levelItemView, levelPack, visualType, packProgress);
-            SubsribeOnClickIfNeed(levelItemView, packIndex, levelPack, visualType, packProgress);
-            
             return levelItemView;
-        }
-
-        private void SubsribeOnClickIfNeed(ILevelItemView levelItemView, int packIndex, LevelPack levelPack, VisualTypeId visualType, LevelPackProgressDictionary packProgress)
-        {
-            int targetLevelIndex;
-            if (visualType is not VisualTypeId.NotOpened)
-            {
-                if (visualType is VisualTypeId.Passed)
-                {
-                    targetLevelIndex = 0;
-                }
-                else
-                {
-                    targetLevelIndex = packProgress.ContainsKey(packIndex)
-                        ? packProgress[packIndex].PassedLevels
-                        : 0;
-                }
-                
-                levelItemView.Clicked += () =>
-                {
-                    _levelPackInfoService.SetData(new LevelPackTransferData()
-                    {
-                        NeedLoadLevel = true,
-                        LevelIndex = targetLevelIndex,
-                        LevelPack = levelPack,
-                        PackIndex = packIndex
-                    });
-                  
-                    _energyService.Dispose();
-                    
-                    _stateMachine.Enter<LoadingSceneState, string, bool>(SceneNaming.Game, false);
-                };
-            }
-        }
-
-        private void ChangeVisual(int packIndex, ILevelItemView levelItemView, LevelPack levelPack, VisualTypeId visualType, LevelPackProgressDictionary packProgress)
-        {
-            LevelItemViewData levelItemViewData = _levelItemViewByTypeProvider.Views[visualType];
-            int passedLevels = packProgress.ContainsKey(packIndex) ? packProgress[packIndex].PassedLevels : 0;
-                
-            levelItemView.UpdateVisual(new()
-            {
-                LevelViewData = levelItemViewData,
-                LevelPack = levelPack,
-                VisualTypeId = visualType,
-                PassedLevels = passedLevels,
-                LocaleKey = visualType is VisualTypeId.NotOpened ? MainSceneLocaleConstants.GalacticNotFoundKey : levelPack.LocaleKey
-            });
-        }
-
-        private VisualTypeId GetVisualType(int packIndex, LevelPack levelPack, LevelPackProgressDictionary packProgress)
-        {
-            if (packIndex == 0 && packProgress.Count == 0)
-            {
-                packProgress.Add(0, new());
-                return VisualTypeId.InProgress;
-            }
-
-            var lastOpenedPack = packProgress.Last();
-
-            if (packIndex == lastOpenedPack.Key + 1 &&
-                lastOpenedPack.Value.PassedLevels >= _levelPackProvider.LevelPacks[lastOpenedPack.Key].Levels.Count)
-            {
-                return VisualTypeId.InProgress;
-            }
-            
-            if (!packProgress.ContainsKey(packIndex))
-            {
-                return VisualTypeId.NotOpened;
-            }
-
-            if (packProgress[packIndex].PassedLevels >= levelPack.Levels.Count)
-            {
-                return VisualTypeId.Passed;
-            }
-
-            return VisualTypeId.InProgress;
         }
     }
 }
--- Assets/App/Scripts/Scenes/MainMenuScene/LevelPacks/MonoBehaviours/LevelPackContainer.cs
+++ Assets/App/Scripts/Scenes/MainMenuScene/LevelPacks/MonoBehaviours/LevelPackContainer.cs
@@ -1,5 +1,13 @@
-﻿using App.Scripts.General.Levels;
-using App.Scripts.Scenes.MainMenuScene.Factories.Levels;
+﻿using System.Linq;
+using App.Scripts.External.GameStateMachine;
+using App.Scripts.External.UserData;
+using App.Scripts.General.Constants;
+using App.Scripts.General.Energy;
+using App.Scripts.General.LevelPackInfoService;
+using App.Scripts.General.Levels;
+using App.Scripts.General.States;
+using App.Scripts.General.UserData.Levels.Data;
+using App.Scripts.Scenes.MainMenuScene.Constants;
 using App.Scripts.Scenes.MainMenuScene.LevelPacks.Configs;
 using UnityEngine;
 using Zenject;
@@ -8,18 +16,119 @@ namespace App.Scripts.Scenes.MainMenuScene.LevelPacks.MonoBehaviours
 {
     public class LevelPackContainer : MonoBehaviour
     {
+        private LevelItemViewByTypeProvider _levelItemViewByTypeProvider;
+        private IStateMachine _stateMachine;
+        private LevelPackProvider _levelPackProvider;
+        private ILevelPackInfoService _levelPackInfoService;
+        private IEnergyService _energyService;
+
         [Inject]
-        private void Construct(LevelPackProvider levelPackProvider, ILevelItemView.Factory levelItemFactory)
+        private void Construct(
+            LevelPackProvider levelPackProvider,
+            ILevelItemView.Factory levelItemFactory,
+            IUserDataContainer userDataContainer,
+            LevelItemViewByTypeProvider levelItemViewByTypeProvider,
+            IStateMachine stateMachine,
+            ILevelPackInfoService levelPackInfoService,
+            IEnergyService energyService)
         {
-            CreateAllLevelPacks(levelPackProvider, levelItemFactory);
+            _energyService = energyService;
+            _levelItemViewByTypeProvider = levelItemViewByTypeProvider;
+            _levelPackProvider = levelPackProvider;
+            _levelPackInfoService = levelPackInfoService;
+            _stateMachine = stateMachine;
+            
+            CreateAllLevelPacks(levelItemFactory, (LevelPackProgressDictionary)userDataContainer.GetData<LevelPackProgressDictionary>());
         }
 
-        private void CreateAllLevelPacks(LevelPackProvider levelPackProvider, ILevelItemView.Factory levelItemFactory)
+        private void CreateAllLevelPacks(ILevelItemView.Factory levelItemFactory, LevelPackProgressDictionary levelPackProgressDictionary)
         {
-            for (int i = 0; i < levelPackProvider.LevelPacks.Count; i++)
+            for (int i = 0; i < _levelPackProvider.LevelPacks.Count; i++)
             {
-                levelItemFactory.Create(i, levelPackProvider.LevelPacks[i]);
+                ILevelItemView levelItemView = levelItemFactory.Create(i, _levelPackProvider.LevelPacks[i]);
+                
+                VisualTypeId visualType = GetVisualType(i, _levelPackProvider.LevelPacks[i], levelPackProgressDictionary);
+            
+                ChangeVisual(i, levelItemView, _levelPackProvider.LevelPacks[i], visualType, levelPackProgressDictionary);
+                SubsribeOnClickIfNeed(levelItemView, i, _levelPackProvider.LevelPacks[i], visualType, levelPackProgressDictionary);
             }
+        }
+        
+        private void SubsribeOnClickIfNeed(ILevelItemView levelItemView, int packIndex, LevelPack levelPack, VisualTypeId visualType, LevelPackProgressDictionary packProgress)
+        {
+            int targetLevelIndex;
+            if (visualType is not VisualTypeId.NotOpened)
+            {
+                if (visualType is VisualTypeId.Passed)
+                {
+                    targetLevelIndex = 0;
+                }
+                else
+                {
+                    targetLevelIndex = packProgress.ContainsKey(packIndex)
+                        ? packProgress[packIndex].PassedLevels
+                        : 0;
+                }
+                
+                levelItemView.Clicked += () =>
+                {
+                    _levelPackInfoService.SetData(new LevelPackTransferData()
+                    {
+                        NeedLoadLevel = true,
+                        LevelIndex = targetLevelIndex,
+                        LevelPack = levelPack,
+                        PackIndex = packIndex
+                    });
+                  
+                    _energyService.Dispose();
+                    
+                    _stateMachine.Enter<LoadingSceneState, string, bool>(SceneNaming.Game, false);
+                };
+            }
+        }
+
+        private void ChangeVisual(int packIndex, ILevelItemView levelItemView, LevelPack levelPack, VisualTypeId visualType, LevelPackProgressDictionary packProgress)
+        {
+            LevelItemViewData levelItemViewData = _levelItemViewByTypeProvider.Views[visualType];
+            int passedLevels = packProgress.ContainsKey(packIndex) ? packProgress[packIndex].PassedLevels : 0;
+                
+            levelItemView.UpdateVisual(new()
+            {
+                LevelViewData = levelItemViewData,
+                LevelPack = levelPack,
+                VisualTypeId = visualType,
+                PassedLevels = passedLevels,
+                LocaleKey = visualType is VisualTypeId.NotOpened ? MainSceneLocaleConstants.GalacticNotFoundKey : levelPack.LocaleKey
+            });
+        }
+
+        private VisualTypeId GetVisualType(int packIndex, LevelPack levelPack, LevelPackProgressDictionary packProgress)
+        {
+            if (packIndex == 0 && packProgress.Count == 0)
+            {
+                packProgress.Add(0, new());
+                return VisualTypeId.InProgress;
+            }
+
+            var lastOpenedPack = packProgress.Last();
+
+            if (packIndex == lastOpenedPack.Key + 1 &&
+                lastOpenedPack.Value.PassedLevels >= _levelPackProvider.LevelPacks[lastOpenedPack.Key].Levels.Count)
+            {
+                return VisualTypeId.InProgress;
+            }
+            
+            if (!packProgress.ContainsKey(packIndex))
+            {
+                return VisualTypeId.NotOpened;
+            }
+
+            if (packProgress[packIndex].PassedLevels >= levelPack.Levels.Count)
+            {
+                return VisualTypeId.Passed;
+            }
+
+            return VisualTypeId.InProgress;
         }
     }
 }
