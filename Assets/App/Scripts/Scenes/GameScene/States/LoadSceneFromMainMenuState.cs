63779437a2159055a9e198e77dc79801617f5e4a
Danila Bladyko <danila.bladyko@stark.games>
2024-05-22 23:57:22 +0300

    Исправил баг с переходом из игровой сцены в главное меню

--- Assets/App/Scripts/Scenes/GameScene/States/LoadSceneFromMainMenuState.cs
+++ Assets/App/Scripts/Scenes/GameScene/States/LoadSceneFromMainMenuState.cs
@@ -1,4 +1,5 @@
 ﻿using App.Scripts.External.GameStateMachine;
+using App.Scripts.General.InfoBetweenScenes;
 using App.Scripts.General.Popup;
 using App.Scripts.General.States;
 using App.Scripts.Scenes.GameScene.Dotween;
@@ -12,16 +13,23 @@ namespace App.Scripts.Scenes.GameScene.States
         private readonly IStateMachine _projectStateMachine;
         private readonly IPopupService _popupService;
         private readonly ITweenersLocator _tweenersLocator;
+        private readonly InfoBetweenScenes _infoBetweenScenes;
 
-        public LoadSceneFromMainMenuState(IStateMachine projectStateMachine, IPopupService popupService, ITweenersLocator tweenersLocator)
+        public LoadSceneFromMainMenuState(
+            IStateMachine projectStateMachine,
+            IPopupService popupService,
+            ITweenersLocator tweenersLocator,
+            InfoBetweenScenes infoBetweenScenes)
         {
             _projectStateMachine = projectStateMachine;
             _popupService = popupService;
             _tweenersLocator = tweenersLocator;
+            _infoBetweenScenes = infoBetweenScenes;
         }
         
         public async UniTask Enter(string sceneName)
         {
+            _infoBetweenScenes.NeedShowLevelPackContainer = true;
             _projectStateMachine.Enter<LoadingSceneState, string, bool>(sceneName, false);
             
             _tweenersLocator.RemoveAll();
--- Assets/App/Scripts/Scenes/MainMenuScene/Installers/MainMenuInstaller.cs
+++ Assets/App/Scripts/Scenes/MainMenuScene/Installers/MainMenuInstaller.cs
@@ -8,22 +8,29 @@ using App.Scripts.General.Popup.AssetManagment;
 using App.Scripts.General.Popup.Factory;
 using App.Scripts.General.RootUI;
 using App.Scripts.General.UserData.Levels.Data;
+using App.Scripts.Scenes.MainMenuScene.ActivateScreens;
 using App.Scripts.Scenes.MainMenuScene.Factories.ItemView;
 using App.Scripts.Scenes.MainMenuScene.Factories.Levels;
 using App.Scripts.Scenes.MainMenuScene.LevelPacks;
 using App.Scripts.Scenes.MainMenuScene.LevelPacks.MonoBehaviours;
 using App.Scripts.Scenes.MainMenuScene.LocaleView;
 using Sirenix.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
 namespace App.Scripts.Scenes.MainMenuScene.Installers
 {
     public class MainMenuInstaller : MonoInstaller
     {
-        [OdinSerialize] public LevelItemView LevelItemView;
-        [OdinSerialize] public LevelPackParent LevelPackParent;
-        [OdinSerialize] public LocaleItemView LocaleItemView;
-        [OdinSerialize] public EnergyView EnergyView;
+        [SerializeField] public LevelItemView LevelItemView;
+        [SerializeField] public LevelPackParent LevelPackParent;
+        [SerializeField] public LocaleItemView LocaleItemView;
+        [SerializeField] public EnergyView EnergyView;
+        [SerializeField] public Button PlayButton;
+        [SerializeField] public Button BackButton;
+        [SerializeField] public GameObject InitialScreen;
+        [SerializeField] public GameObject LevelPacksScreen;
 
         [Inject] private RootUIViewProvider _rootUIView;
         
@@ -42,7 +49,11 @@ namespace App.Scripts.Scenes.MainMenuScene.Installers
             Container.Bind<IPopupProvider>().To<ResourcesPopupProvider>().AsSingle();
             Container.Bind<IPopupFactory>().To<PopupFactory>().AsSingle();
             Container.Bind<IPopupService>().To<PopupService>().AsSingle().WithArguments(_rootUIView.BackPopupPlane);
-            
+
+            Container
+                .BindInterfacesAndSelfTo<ActivateScreensService>()
+                .AsSingle()
+                .WithArguments(PlayButton, BackButton, InitialScreen, LevelPacksScreen);
             
             Container
                 .BindFactory<int, LevelPack, ILevelItemView, ILevelItemView.Factory>()
