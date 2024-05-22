c2379f54ae61c6af978b94e2a00695017d27338a
Danila Bladyko <danila.bladyko@stark.games>
2024-05-22 15:19:55 +0300

    Поправил получение урона

--- Assets/App/Scripts/Scenes/GameScene/Ball/Collision/BallCollisionService.cs
+++ Assets/App/Scripts/Scenes/GameScene/Ball/Collision/BallCollisionService.cs
@@ -3,6 +3,7 @@ using App.Scripts.Scenes.GameScene.Effects;
 using App.Scripts.Scenes.GameScene.Entities;
 using App.Scripts.Scenes.GameScene.Healthes;
 using App.Scripts.Scenes.GameScene.Levels.View;
+using App.Scripts.Scenes.GameScene.ScreenInfo;
 using UnityEngine;
 
 namespace App.Scripts.Scenes.GameScene.Ball.Collision
@@ -14,26 +15,33 @@ namespace App.Scripts.Scenes.GameScene.Ball.Collision
         private readonly IHealthContainer _healthContainer;
         private readonly CircleEffect.Factory _circleEffectFactory;
         private readonly ILevelViewUpdater _levelViewUpdater;
+        private readonly IScreenInfoProvider _screenInfoProvider;
+        private readonly float _minBallYPosition;
 
         public BallCollisionService(
             IRigidablebody ball,
             IPositionable playerView,
             IHealthContainer healthContainer,
             CircleEffect.Factory circleEffectFactory,
-            ILevelViewUpdater levelViewUpdater)
+            ILevelViewUpdater levelViewUpdater,
+            IScreenInfoProvider screenInfoProvider)
         {
             _ball = ball;
             _playerView = playerView;
             _healthContainer = healthContainer;
             _circleEffectFactory = circleEffectFactory;
             _levelViewUpdater = levelViewUpdater;
+            _screenInfoProvider = screenInfoProvider;
+
+            _minBallYPosition = (_ball as ISpriteRenderable).SpriteRenderer.bounds.size.y -
+                                _screenInfoProvider.HeightInWorld / 2f;
 
             _ball.Collidered += OnCollidered;
         }
 
         private void OnCollidered(Collider2D collider)
         {
-            if (_ball.Position.y < _playerView.Position.y)
+            if (_ball.Position.y <= _minBallYPosition)
             {
                 _healthContainer.UpdateHealth(-1);
             }
