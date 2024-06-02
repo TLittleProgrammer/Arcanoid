2c24864e97e58831723493914735e75e900bab00
Danila Bladyko <danila.bladyko@stark.games>
2024-06-02 18:17:59 +0300

    Поравил паки на стартовой сцене

--- Assets/App/Scripts/Scenes/MainMenuScene/Features/LevelPacks/ILevelItemView.cs
+++ Assets/App/Scripts/Scenes/MainMenuScene/Features/LevelPacks/ILevelItemView.cs
@@ -1,6 +1,7 @@
 ﻿using App.Scripts.External.Components;
 using App.Scripts.External.Localisation.MonoBehaviours;
 using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
 
@@ -13,14 +14,14 @@ namespace App.Scripts.Scenes.MainMenuScene.LevelPacks
         Image BigBackground { get; }
         TMP_Text GalacticText { get; }
         UILocale GalacticName { get; }
+        UILocale SubTextLocale { get; }
         Image Glow { get; }
         TMP_Text EnergyText { get; }
         Image LeftImageHalf { get; }
         Image GalacticIcon { get; }
         Image LockIcon { get; }
         Image MaskableImage { get; }
-
-        void UpdateVisual(LevelModel levelModel);
+        GameObject EnergyPanel { get; }
         
         public class Factory : PlaceholderFactory<ILevelItemView>
         {
