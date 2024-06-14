844ba515c695775ffe21cd4c53ce57d56af9e52a
Danila Bladyko <danila.bladyko@stark.games>
2024-06-15 02:05:11 +0300

    Добавил тесты. Поправил корректировку движения шарика согласно документации

--- Assets/App/Scripts/Scenes/GameScene/Features/Settings/BallFlyingSettings.cs
+++ Assets/App/Scripts/Scenes/GameScene/Features/Settings/BallFlyingSettings.cs
@@ -5,7 +5,6 @@ namespace App.Scripts.Scenes.GameScene.Features.Settings
     [CreateAssetMenu(menuName = "Configs/Game Settings/Ball Flying", fileName = "BallFlying")]
     public class BallFlyingSettings : ScriptableObject
     {
-        public float MaxAngle;
         public float MinAngle;
         public float Speed;
     }
---
+++ Assets/App/Scripts/Tests/Math/MathService.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace App.Scripts.Tests.Math
+{
+    public static class MathService
+    {
+        public static Vector2 GetDirectionByAngle(float angle)
+        {
+            float radians = angle * Mathf.Deg2Rad;
+            
+            return new Vector2(Mathf.Cos(radians), Mathf.Sin(radians));
+        }
+    }
+}
