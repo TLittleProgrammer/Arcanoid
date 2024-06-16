cdb4f7bc45b7a1a3a4275280a79be222d5cc9bfb
Danila Bladyko <danila.bladyko@stark.games>
2024-06-16 19:22:41 +0300

    Долил по тестам, что сразу не подлилось

--- Assets/App/Scripts/Tests/Healthes/ChangeHealthesTest.cs
+++ Assets/App/Scripts/Tests/Healthes/ChangeHealthesTest.cs
@@ -1,7 +1,37 @@
-﻿namespace App.Scripts.Tests.Healthes
+﻿using App.Scripts.Scenes.GameScene.Features.Healthes;
+using Cysharp.Threading.Tasks;
+using NUnit.Framework;
+
+namespace App.Scripts.Tests.Healthes
 {
-    public class ChangeHealtesTest
+    public class ChangeHealthesTest
     {
+        private IHealthContainer _healthContainer;
         
+        [SetUp]
+        public void SetUp()
+        {
+            IViewHealthPointService viewHealthPointService = new TestViewHealthPointService();
+            _healthContainer = new HealthContainer(viewHealthPointService);
+        }
+
+        [Test]
+        [TestCase(3, -1, 2)]
+        [TestCase(3, -2, 1)]
+        [TestCase(3, -3, 0)]
+        [TestCase(3, -4, -1)]
+        [TestCase(3, -5, -1)]
+        [TestCase(3, -999, -1)]
+        [TestCase(3, 999, 3)]
+        [TestCase(5, 9, 5)]
+        [TestCase(12, 1, 12)]
+        public void ChangeHealthTest(int initialHealthes, int addHealth, int needResult)
+        {
+            _healthContainer.AsyncInitialize(initialHealthes).Forget();
+            
+            _healthContainer.UpdateHealth(addHealth);
+
+            Assert.AreEqual(needResult, _healthContainer.CurrentHealthPoints);
+        }
     }
 }
--- Assets/App/Scripts/Tests/Healthes/TestViewHealthPointService.cs
+++ Assets/App/Scripts/Tests/Healthes/TestViewHealthPointService.cs
@@ -1,7 +1,23 @@
-﻿namespace App.Scripts.Tests.Healthes
+﻿using App.Scripts.Scenes.GameScene.Features.Healthes;
+using App.Scripts.Scenes.GameScene.Features.Levels.General;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace App.Scripts.Tests.Healthes
 {
-    public class TestViewHealthPointService
+    public class TestViewHealthPointService : IViewHealthPointService
     {
-        
+        public UniTask AsyncInitialize(LevelData param)
+        {
+            return UniTask.CompletedTask;
+        }
+
+        public void Restart()
+        {
+        }
+
+        public void UpdateHealth(int healthCount)
+        {
+        }
     }
 }
